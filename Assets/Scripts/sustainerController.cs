2a1e05746f7d66409a0aff21e620265cfe31fda4
Yuki Ishiguro <kuro512@gmail.com>
2021-02-23 23:06:06 +0900

    update
    テストでコインを設置したり、上方向にも移動できるようにしてみた。
    足音はまだならない

--- Assets/Scripts/sustainerController.cs
+++ Assets/Scripts/sustainerController.cs
@@ -3,9 +3,14 @@ using UnityEngine;
 using UnityEngine.UI;
 using Photon.Pun;
 using Photon.Realtime;
+using UnityEngine.EventSystems;
 
 public class sustainerController : MonoBehaviour
 {
+
+    // 地面の位置
+    private float groundLevel = 0.5f;
+
 
     //オンライン化に必要なコンポーネントを設定
     public PhotonView myPV;
@@ -48,6 +53,7 @@ public class sustainerController : MonoBehaviour
     {
         if (myPV.IsMine)    //自キャラであれば実行
         {
+
             //Animatorコンポーネントを取得
             this.myAnimator = GetComponent<Animator>();
 
@@ -56,6 +62,56 @@ public class sustainerController : MonoBehaviour
 
             //Rigidbodyコンポーネントを取得（追加）
             this.myRigidbody = GetComponent<Rigidbody>();
+
+            //RunボタンのPointerDownにGetMyRunButtonDownを登録
+            EventTrigger Run_trigger = GameObject.Find("RunButton").GetComponent<EventTrigger>();
+            EventTrigger.Entry entry = new EventTrigger.Entry();
+            entry.eventID = EventTriggerType.PointerDown;
+            entry.callback.AddListener(data => GetMyRunButtonDown());
+            Run_trigger.triggers.Add(entry);
+            //RunButton のUpで止まる
+            EventTrigger.Entry entry2 = new EventTrigger.Entry();
+            entry2.eventID = EventTriggerType.PointerUp;
+            entry2.callback.AddListener(data => GetMyRunButtonUp());
+            Run_trigger.triggers.Add(entry2);
+
+            //Leftボタン
+            EventTrigger Left_trigger = GameObject.Find("LeftButton").GetComponent<EventTrigger>();
+            EventTrigger.Entry Left_entry = new EventTrigger.Entry();
+            Left_entry.eventID = EventTriggerType.PointerDown;
+            Left_entry.callback.AddListener(data => GetMyLeftButtonDown());
+            Left_trigger.triggers.Add(Left_entry);
+            //LeftButton のUpで止まる
+            EventTrigger.Entry Left_entry2 = new EventTrigger.Entry();
+            Left_entry2.eventID = EventTriggerType.PointerUp;
+            Left_entry2.callback.AddListener(data => GetMyLeftButtonUp());
+            Left_trigger.triggers.Add(Left_entry2);
+
+            //Rightボタン
+            EventTrigger Right_trigger = GameObject.Find("RightButton").GetComponent<EventTrigger>();
+            EventTrigger.Entry Right_entry = new EventTrigger.Entry();
+            Right_entry.eventID = EventTriggerType.PointerDown;
+            Right_entry.callback.AddListener(data => GetMyRightButtonDown());
+            Right_trigger.triggers.Add(Right_entry);
+            //LeftButton のUpで止まる
+            EventTrigger.Entry Right_entry2 = new EventTrigger.Entry();
+            Right_entry2.eventID = EventTriggerType.PointerUp;
+            Right_entry2.callback.AddListener(data => GetMyRightButtonUp());
+            Right_trigger.triggers.Add(Right_entry2);
+
+
+            //JumpボタンのPointerDownにGetMyJButtonDownを登録
+            EventTrigger J_trigger = GameObject.Find("JumpButton").GetComponent<EventTrigger>();
+            EventTrigger.Entry Jump_entry = new EventTrigger.Entry();
+            Jump_entry.eventID = EventTriggerType.PointerDown;
+            Jump_entry.callback.AddListener(data => GetMyJumpButtonDown());
+            J_trigger.triggers.Add(Jump_entry);
+            //JumButton のUpで止まる
+            EventTrigger.Entry Jump_entry2 = new EventTrigger.Entry();
+            Jump_entry2.eventID = EventTriggerType.PointerUp;
+            Jump_entry2.callback.AddListener(data => GetMyJumpButtonUp());
+            J_trigger.triggers.Add(Jump_entry2);
+
         }
 
     }
@@ -65,6 +121,8 @@ public class sustainerController : MonoBehaviour
     void Update()
     {
 
+
+
         //キャラに速度を与える（勝手に走らせるため速度を与える）
         //this.myRigidbody.velocity = new Vector3(0, 0, this.velocityZ);
 
@@ -73,13 +131,15 @@ public class sustainerController : MonoBehaviour
         {
             return;
         }
+
+
+
         //上方向の入力による速度（追加）
         float inputVelocityY = 0;
 
         //ジャンプしていない時にスペースが押されたらジャンプする（追加）
         if ((Input.GetKeyDown(KeyCode.Space) || this.isJButtonDown) && this.transform.position.y < 0.5f)
         {
-            Debug.Log("aaa");
             //ジャンプアニメを再生（追加）
             this.myAnimator.SetBool("Jump", true);
             //上方向への速度を代入（追加）
@@ -88,7 +148,7 @@ public class sustainerController : MonoBehaviour
         else
         {
             //現在のY軸の速度を代入（追加）
-            //inputVelocityY = this.myRigidbody.velocity.y;
+            inputVelocityY = this.myRigidbody.velocity.y;
         }
 
         //Jumpステートの場合はJumpにfalseをセットする（追加）
@@ -98,7 +158,9 @@ public class sustainerController : MonoBehaviour
         }
 
 
-
+        // 着地しているかどうかを調べる
+        //Debug.Log(this.transform.position.y);
+        bool isGround = (transform.position.y > this.groundLevel) ? false : true;
 
 
         //float inputVelocityX = Input.GetAxis("Horizontal");
@@ -109,12 +171,22 @@ public class sustainerController : MonoBehaviour
             this.myAnimator.SetFloat("Speed", 1);
             transform.position += transform.forward * 0.05f;
             myAnimator.SetBool("walk", true);
+
+            GetComponent<AudioSource>().volume = (isGround) ? 1 : 0;
+            //this.myAnimator.SetBool("isGround", isGround);
+            Debug.Log(isGround);
+
         }
         else
         {
             //走るのをやめる
             this.myAnimator.SetFloat("Speed", 0);
             myAnimator.SetBool("walk", false);
+            //ボリュームを0にする
+            GetComponent<AudioSource>().volume = 0;
+
+            //this.myAnimator.SetBool("isGround", isGround);
+            Debug.Log(isGround);
         }
 
 
@@ -184,5 +256,25 @@ public class sustainerController : MonoBehaviour
     public void GetMyRightButtonUp()
     {
         this.isRButtonDown = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        //コインに衝突した場合
+        if (other.gameObject.tag == "CoinTag")
+        {
+            //スコアを加算
+            //this.score += 10;
+
+            //ScoreTextに獲得した点数を表示
+            //this.scoreText.GetComponent<Text>().text = "Score " + this.score + "pt";
+
+            //パーティクルの再生
+            GetComponent<ParticleSystem>().Play();
+
+            //接触したコインのオブジェクトを破棄
+            Destroy(other.gameObject);
+        }
     }
 }
